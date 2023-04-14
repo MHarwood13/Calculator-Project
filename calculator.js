7c600b88090b0ad462045a5785cae38acea64ad6
Michael <mharwood13@gmail.com>
2023-04-14 12:18:40 -0400

    calc kind of done

--- calculator.js
+++ calculator.js
@@ -1,136 +1,90 @@
-// Select all the calculator buttons and the display input
-const buttons = document.querySelectorAll('.button');
+
 const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
 
-// Define a variable to hold the calculator's current state
-let state = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null
-};
+let operand1 = '';
+let operand2 = '';
+let operator = '';
 
-// Function to update the display with the current value
-function updateDisplay() {
-  display.value = state.displayValue;
+buttons.forEach(button => {
+  button.addEventListener('click', handleClick);
+});
+
+function handleClick(e) {
+  const value = e.target.value;
+
+  if (value === 'C') {
+    clear();
+  } else if (value === '←') {
+    deleteLast();
+  } else if (isNumber(value) || value === '.') {
+    handleNumber(value);
+  } else if (isOperator(value)) {
+    handleOperator(value);
+  } else if (value === '=') {
+    handleEquals();
+  }
+
+  display.value = operand1;
 }
 
-// Function to handle input digit clicks
-function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = state;
+function clear() {
+  operand1 = '';
+  operand2 = '';
+  operator = '';
+}
 
-  // Update the display value based on the current state
-  if (waitingForSecondOperand === true) {
-    state.displayValue = digit;
-    state.waitingForSecondOperand = false;
+function deleteLast() {
+  if (operator) {
+    operand2 = operand2.slice(0, -1);
+    display.value = operand2;
   } else {
-    state.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    operand1 = operand1.slice(0, -1);
+    display.value = operand1;
   }
-
-  // Update the display and the state
-  updateDisplay();
 }
 
-// Function to handle decimal point clicks
-function inputDecimal(dot) {
-  const { displayValue, waitingForSecondOperand } = state;
-
-  // Add a decimal point if one is not already present
-  if (waitingForSecondOperand === true) {
-    state.displayValue = '0.';
-    state.waitingForSecondOperand = false;
-    return;
-  }
-
-  if (!displayValue.includes(dot)) {
-    state.displayValue += dot;
-  }
-
-  // Update the display and the state
-  updateDisplay();
+function isNumber(value) {
+  return !isNaN(value);
 }
 
-// Function to handle operator clicks
-function handleOperator(nextOperator) {
-  const { firstOperand, displayValue, operator } = state;
-  const inputValue = parseFloat(displayValue);
-
-  if (operator && state.waitingForSecondOperand) {
-    state.operator = nextOperator;
-    return;
-  }
-
-  if (firstOperand == null && !isNaN(inputValue)) {
-    state.firstOperand = inputValue;
-  } else if (operator) {
-    const result = calculate(firstOperand, inputValue, operator);
-
-    state.displayValue = `${parseFloat(result.toFixed(7))}`;
-    state.firstOperand = result;
-  }
-
-  state.waitingForSecondOperand = true;
-  state.operator = nextOperator;
+function isOperator(value) {
+  return value === '+' || value === '-' || value === 'x' || value === '÷';
 }
 
-// Function to perform calculations
-function calculate(firstOperand, secondOperand, operator) {
+function handleNumber(value) {
+  if (operator) {
+    operand2 += value;
+    display.value = operand2;
+  } else {
+    operand1 += value;
+    display.value = operand1;
+  }
+}
+
+function handleOperator(value) {
+  operator = value;
+}
+
+function handleEquals() {
+  const num1 = parseFloat(operand1);
+  const num2 = parseFloat(operand2);
+
   switch (operator) {
     case '+':
-      return firstOperand + secondOperand;
+      operand1 = num1 + num2;
+      break;
     case '-':
-      return firstOperand - secondOperand;
-    case '*':
-      return firstOperand * secondOperand;
-    case '/':
-      return firstOperand / secondOperand;
-    default:
-      return secondOperand;
+      operand1 = num1 - num2;
+      break;
+    case 'x':
+      operand1 = num1 * num2;
+      break;
+    case '÷':
+      operand1 = num1 / num2;
+      break;
   }
+
+  operand2 = '';
+  operator = '';
 }
-
-// Function to reset the calculator
-function resetCalculator() {
-  state = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null
-  };
-  updateDisplay();
-}
-
-// Add event listeners to each calculator button
-buttons.forEach(button => {
-  button.addEventListener('click', event => {
-    const { target } = event;
-    const { value } = target;
-
-    if (!target.matches('button')) {
-      return;
-    }
-
-    switch (value) {
-      case '+':
-      case '-':
-      case '*':
-      case '/':
-      case '=':
-        handleOperator(value);
-        break;
-      case '.':
-        inputDecimal(value);
-        break;
-      case 'C':
-        resetCalculator();
-        break;
-      default:
-        if (Number.isInteger(parseFloat(value))) {
-          inputDigit(value);
-        }
-    }
-
-    // Update the display on every button click
-    updateDisplay();
-  });
-});
